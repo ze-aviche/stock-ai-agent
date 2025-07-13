879bdcf7225857765a7cf5fd74ef5f0fd4f7d9f9
Avinash Chennamadhav <avinashchennamadhav@Avinashs-Air.lan>
2025-07-13 18:42:42 -0500

    added sample stock price agent

---
+++ trading_advisor/__init__.py
@@ -0,0 +1,2 @@
+from . import agent
+#from .trading_advisor_agent import root_agent
---
+++ trading_advisor/agent.py
@@ -0,0 +1,28 @@
+from google.adk.agents import Agent
+
+def get_stock_price(stock_symbol: float) -> dict[str, str]:
+    """Get the current price of a stock"""
+    if stock_symbol == "AAPL":
+        return {"status": "success",
+                "message": "Stock price for AAPL is 100"
+                }
+
+    else:
+        return {
+            "error": "Stock symbol not found",
+            "error_message": f"Stock symbol {stock_symbol} not found"
+        }
+
+
+root_agent = Agent(
+    name="trading_advisor_agent",
+    description="An AI agent that analyzes stock data and provides trading recommendations",
+    tools=[get_stock_price],
+    model="gemini-2.0-flash",
+    
+)
+
+
+
+
+
